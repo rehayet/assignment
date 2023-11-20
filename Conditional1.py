a6b521255870f5f8054dd14959ea0cbcff9ad8b5
Rehayet Hossen Shakil <rehayethossenshakil@Rehayets-MacBook-Air.local>
2023-11-20 13:54:37 +0600

    Rehayet

---
+++ Conditional1.py
@@ -0,0 +1,31 @@
+a = 10
+b = 20
+
+if(a==b):
+    print("a is equal to b")
+else:
+    print("something went wrong")
+
+
+
+totalPrice = 501
+discount = 5
+if(totalPrice>500):
+    totalPrice = totalPrice - ((totalPrice*discount)/100)
+    print(totalPrice)
+else:
+    print(totalPrice)
+
+age = 17
+if(age<18):
+    print("You are not able to register for NID")
+else:
+    print("You are eligible")
+
+name = ""
+sizeName = len(name)
+if(sizeName==0):
+    print("The field name can not be empty")
+else:
+    print("Success")
+
---
+++ HomeWork.py
@@ -0,0 +1,7 @@
+phoneNumber = "01500000000"
+phoneDigits = len(phoneNumber)
+if(phoneDigits==11):
+    print("Success")
+else:
+    print("The phone number must be with 11 digits")
+    
